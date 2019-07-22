fa6be505a0a5d5729f437c745424a85b881f854c
djs <danieljsalazar2@icloud.com>
2019-07-21 21:34:50 -0700

    feat: Updated Serverless Udagram
    Separate business logic from createGroup and getGroups functions
    Added classes and interfaces for a Group object

--- Serverless Framework/src/lambda/http/createGroup.ts
+++ Serverless Framework/src/lambda/http/createGroup.ts
@@ -1,33 +1,17 @@
 import {APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult} from "aws-lambda";
 import 'source-map-support/register'
-import * as uuid from 'uuid';
-import * as AWS from 'aws-sdk';
-import {getUserId} from "../../auth/utils";
-
-const docClient = new AWS.DynamoDB.DocumentClient();
-const groupsTable = process.env.GROUPS_TABLE;
+import {CreateGroupRequest} from "../../requsts/CreateGroupRequest";
+import {createGroup} from "../../businessLogic/groups";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event);
-  // Generate a random id
-  const itemId = uuid.v4();
-  // Body of request will contain the elements to be added
-  const parsedBody = JSON.parse(event.body);
-
+  const newGroup: CreateGroupRequest = JSON.parse(event.body);
   const authorization = event.headers.Authorization;
   const split = authorization.split(' ');
   const jwtToken = split[1];
 
-  const newItem = {
-    id: itemId,
-    userId: getUserId(jwtToken),
-    ...parsedBody
-  };
-  // Create a new item in DynamoDB
-  await docClient.put({
-    TableName: groupsTable,
-    Item: newItem
-  }).promise();
+  const newItem = await createGroup(newGroup, jwtToken);
+
   // Return results
   return {
     statusCode: 201,
